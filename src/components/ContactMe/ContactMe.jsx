197a3d5c218774c3001bc440465bfc9d3d0859b4
VentusTWY <ventustwy21@gmail.com>
2022-09-21 17:04:33 +0800

    modularised contact me form component

--- src/components/ContactMe/ContactMe.jsx
+++ src/components/ContactMe/ContactMe.jsx
@@ -2,16 +2,10 @@ import { Text, Box, Flex, Input, Button } from '@chakra-ui/react'
 import React from 'react'
 import { HeaderBar } from '../HeaderBar/HeaderBar'
 import { WIPCard } from '../WIPCard'
-import { FormControl, FormLabel, FormErrorMessage, FormHelperText } from '@chakra-ui/react'
-import { useForm, ValidationError } from '@formspree/react'
+
+import { ContactMeForm } from './ContactMeForm'
 
 export const ContactMe = () => {
-  const [state, handleSubmit] = useForm('mnqrpkla')
-
-  if (state.succeeded) {
-    return <Text>Thank you for your message! You will receive a reply shortly!</Text>
-  }
-
   return (
     <>
       <HeaderBar />
@@ -27,41 +21,7 @@ export const ContactMe = () => {
         <Text color={'black'}>
           Feel free to reach out if you have any questions or just want a chat!
         </Text>
-
-        <Flex borderColor={'#001C55'} borderWidth={'thick'} width={'80%'} p={20} borderRadius={8}>
-          {/* <form onSubmit={handleSubmit}> */}
-          <FormControl onSubmit={handleSubmit} isRequired width={'66vw'} my={'-40px'}>
-            <FormLabel mt={'-10px'}>Email address</FormLabel>
-            <Input borderColor={'#001C55'} borderWidth={'medium'} type='email' id='email' />
-            <ValidationError prefix='Email' field='email' errors={state.errors} />
-            <FormHelperText my={'5px'}>
-              Don't worry, your email will not be shared out :D
-            </FormHelperText>
-
-            <FormLabel my={'20px'}>Name</FormLabel>
-            <Input borderColor={'#001C55'} borderWidth={'medium'} type='name' id='name' />
-
-            <FormLabel my={'20px'}>Message</FormLabel>
-            <Input borderColor={'#001C55'} borderWidth={'medium'} type='message' id='message' />
-
-            <Button
-              width={'100%'}
-              mt={'20px'}
-              bgColor={'#001C55'}
-              color={'white'}
-              type='submit'
-              disabled={state.submitting}
-              _hover={{
-                color: 'white',
-                cursor: 'pointer',
-                bgGradient: 'linear(to-tl, blue.200, #2B528D)',
-              }}
-            >
-              Submit
-            </Button>
-          </FormControl>
-          {/* </form> */}
-        </Flex>
+        <ContactMeForm />
       </Flex>
       {/* <WIPCard /> */}
     </>
---
+++ src/components/ContactMe/ContactMeForm.jsx
@@ -0,0 +1,48 @@
+import { Text, Box, Flex, Input, Button } from '@chakra-ui/react'
+import { FormControl, FormLabel, FormErrorMessage, FormHelperText } from '@chakra-ui/react'
+import { useForm, ValidationError } from '@formspree/react'
+
+export const ContactMeForm = () => {
+  const [state, handleSubmit] = useForm('mnqrpkla')
+
+  if (state.succeeded) {
+    return <Text>Thank you for your message! You will receive a reply shortly!</Text>
+  }
+
+  return (
+    <Flex borderColor={'#001C55'} borderWidth={'thick'} width={'80%'} p={20} borderRadius={8}>
+      {/* <form onSubmit={handleSubmit}> */}
+      <FormControl onSubmit={handleSubmit} isRequired width={'66vw'} my={'-40px'}>
+        <FormLabel mt={'-10px'}>Email address</FormLabel>
+        <Input borderColor={'#001C55'} borderWidth={'medium'} type='email' id='email' />
+        {/* <ValidationError prefix='Email' field='email' errors={state.errors} /> */}
+        <FormHelperText my={'5px'}>
+          Don't worry, your email will not be shared out :D
+        </FormHelperText>
+
+        <FormLabel my={'20px'}>Name</FormLabel>
+        <Input borderColor={'#001C55'} borderWidth={'medium'} type='name' id='name' />
+
+        <FormLabel my={'20px'}>Message</FormLabel>
+        <Input borderColor={'#001C55'} borderWidth={'medium'} type='message' id='message' />
+
+        <Button
+          width={'100%'}
+          mt={'20px'}
+          bgColor={'#001C55'}
+          color={'white'}
+          type='submit'
+          disabled={state.submitting}
+          _hover={{
+            color: 'white',
+            cursor: 'pointer',
+            bgGradient: 'linear(to-tl, blue.200, #2B528D)',
+          }}
+        >
+          Submit
+        </Button>
+      </FormControl>
+      {/* </form> */}
+    </Flex>
+  )
+}
