47c89b27350e3bffc3ccb79d24004dc28c336c8f
VentusTWY <ventustwy21@gmail.com>
2022-11-26 01:50:08 +0000

    changed homepage component name  from layout to home

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
-import { Layout } from './components/Layout/Layout'
+import { Home } from './components/Home/Home'
 import { More } from './components/More/More'
 import { TechnologyExperience } from './components/TechnologyExperience/TechnologyExperience'
 import { ContactMe } from './components/ContactMe/ContactMe'
@@ -10,10 +10,11 @@ function App() {
   return (
     <ChakraProvider>
       <Routes>
-        <Route path='/' element={<Layout />}></Route>
+        <Route path='/' element={<Home />}></Route>
         <Route path='/more' element={<More />}></Route>
         <Route path='/tech' element={<TechnologyExperience />}></Route>
         <Route path='/contactme' element={<ContactMe />}></Route>
+        {/* <Route path='/resume' element={<Resume />}></Route> */}
       </Routes>
     </ChakraProvider>
   )
